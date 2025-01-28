25d5a406ea8786052ed482125894322698948606
Abdelrhman Atta <abdelrhmatta@gmail.com>
2025-01-28 11:26:33 +0200

    Added uart 1.0.1

--- uart/uart.c
+++ uart/uart.c
@@ -1,11 +1,30 @@
+/**
+ * @file	UART.c
+ * @version	1.0.1
+ * @brief	UART protocol source file for AVR.
+ * @details	This file include essential functions for AVR to perform UART communication.
+ * @author	A.Atta
+ * @date	24/01/2025
+ **/
+
+
+/*========			Linking Section Start			========*/
+
 #include <avr/io.h>
+
 #include "uart.h"
 
-/*
- * 8-N-1 Format.
- * Takes 'ubrr' as a parameter.
- * 'ubrr' is baud rate prescaler defined in header file.
- */
+/*========			Linking Section End			========*/
+
+
+/*========			Function Definition Section Start	========*/
+
+/**
+ * Function:	UART_init(usigned int);
+ * Use:		Intilaise UART0 registers.
+ * Input:	unsigned int ubrr (baud rate prescaler value)
+ * Output:	void
+ **/
 void UART_init(unsigned int ubrr)
 {
 	UBRR0H = (unsigned char)(ubrr >> 8);
@@ -15,22 +34,38 @@ void UART_init(unsigned int ubrr)
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-/*
- * Transmits unsigned characters.
- * Takes 'ch' as a parameter.
- * 'ch' is the desired unsigned character to send.
- */
+/**
+ * Function:	UART_TxChar(unsigned char);
+ * Use:		Transmits unsigned character
+ * Input:	unsigned character 'ch'
+ * Output:	void
+ **/
 void UART_TxChar(unsigned char ch)
 {
 	while(!((UCSR0A) & (1 << UDRE0)));
 	UDR0 = ch;
 }
 
-/*
- * Receives unsigned characters.
- */
+/**
+ * Function:	UART_RxChar(void);
+ * Use:		Recives unsigned character.
+ * Input:	void
+ * Output:	unsigned character
+ **/
 unsigned char UART_RxChar(void)
 {
 	while(!((UCSR0A) & (1 << RXC0)));
 	return UDR0;
 }
+
+/*========			Definition Defention Section End	========*/
+
+
+/*================================================================================
+ * Change Log.
+ * 
+ * Date			By		Description
+ * ----------		--------	------------------------------------------
+ * 28/01/2025		A.Atta		Added comments for clarity.
+ */
+
--- uart/uart.h
+++ uart/uart.h
@@ -1,14 +1,58 @@
 #ifndef UART_H
 #define UART_H
 
-#define F_CPU	8000000UL
+
+/**
+ * @file    uart.h
+ * @version 1.0.1
+ * @brief   UART communication functions for AVR.
+ * @details This header file contains the function prototypes and macro definitions 
+ *          for UART communication for the AVR microcontroller.
+ * @author  A.Atta
+ * @date    24/01/2025
+ **/
+
+
+/*========			Linking Section Start				========*/
+
+#include <avr/io.h>
+
+/*========			Linking Section End				========*/
+
+
+/*========			Definitions Section Start			========*/
+
 #define UART_BAUD_RATE	9600
-#define BAUD_PRESCALE	( ( F_CPU/(16UL * UART_BAUD_RATE) ) - 1)	// Configures UBRRn prescaler value.
+
+#define BAUD_PRESCALE	( ( F_CPU/(16UL * UART_BAUD_RATE) ) - 1)	/* Baud Rate Prescaler */
+
+/*========			Definitions Section End				========*/
 
 
+/*========			Functions Declarations Section Start		========*/
+
+/**
+ * @brief	Function to initialize UART0 registers.
+ * @param	unsigned int
+ * @return	void
+ **/
 void UART_init(unsigned int);
-void UART_TxChar(unsigned char);					// Transmit Characters.
-unsigned char UART_RxChar(void);					// Recives Characters.
+
+/**
+ * @brief	Function to transmit unsigned character using UART0.
+ * @param	unsigned char
+ * @return	void
+ **/
+void UART_TxChar(unsigned char);
+
+/**
+ * @brief	Function to receive unsigned character using UART0.
+ * @param	void
+ * @return	UDR0 "received character" 
+ **/
+unsigned char UART_RxChar(void);
+
+/*========			Functions Declarations Section End		========*/
 
 
-#endif
+#endif /* UART_H */
